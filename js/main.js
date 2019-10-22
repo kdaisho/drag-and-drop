751949ab124042a6ed2d896a9b4b6010699d8c8d
kdaisho <daishokomiyama@gmail.com>
2019-10-22 11:05:02 -0400

    Grabbing done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-const fill = document.querySelector(".fill");
+// const fill = document.querySelector(".fill");
+const fills = document.querySelectorAll(".fill");
 const empties = document.querySelectorAll(".empty");
 
-fill.addEventListener("dragstart", dragStart);
-fill.addEventListener("dragend", dragEnd);
+// fill.addEventListener("dragstart", dragStart);
+// fill.addEventListener("dragend", dragEnd);
+
+
+for (let i = 0; i < fills.length; i++) {
+    fills[i].addEventListener('dragstart', dragStart);
+    fills[i].addEventListener('dragend', dragEnd);
+}
 
 for (let i = 0; i < empties.length; i++) {
     empties[i].addEventListener('dragover', dragOver);
@@ -11,10 +18,14 @@ for (let i = 0; i < empties.length; i++) {
     empties[i].addEventListener('drop', dragDrop);
 }
 
+let grabbed = '';
+
 function dragStart () {
     console.log('start');
-    // this.classList.add("hold");
-    // setTimeout(() => this.className = "invisible", 0);
+    this.classList.add("hold");
+    grabbed = this;
+    console.log(grabbed);
+    setTimeout(() => this.className = "invisible", 0);
 }
 
 function dragEnd () {
@@ -37,58 +48,58 @@ function dragLeave () {
 
 function dragDrop () {
     this.className = "empty";
-    this.append(fill);
+    this.append(grabbed);
 }
 
 //touch device
-const coordinates = [];
-for (let i = 0; i < empties.length; i++) {
-    coordinates.push(empties[i].getBoundingClientRect());
-}
-console.log(coordinates);
+// const coordinates = [];
+// for (let i = 0; i < empties.length; i++) {
+//     coordinates.push(empties[i].getBoundingClientRect());
+// }
+// console.log(coordinates);
 
-fill.addEventListener("touchstart", touchStart);
-fill.addEventListener("touchmove", touchMove, {passive: false});
-fill.addEventListener("touchend", touchEnd);
+// fill.addEventListener("touchstart", touchStart);
+// fill.addEventListener("touchmove", touchMove, {passive: false});
+// fill.addEventListener("touchend", touchEnd);
 
-let offsetX = 100;
-let offsetY = 150;
-let lastPosition = 0;
+// let offsetX = 100;
+// let offsetY = 150;
+// let lastPosition = 0;
 
-const height = 150;
-const width = 150;
+// const height = 150;
+// const width = 150;
 
-function touchStart (event) {
-    const initialLocation = event.targetTouches[0];
-    this.style.position = "relative";
-    this.style.zIndex = 100;
-    offsetX = this.getBoundingClientRect().x;
-    offsetY = this.getBoundingClientRect().y;
-    this.style.left = (initialLocation.pageX - offsetX) + 'px';
-    this.style.top = (initialLocation.pageY - offsetY) + 'px';
-    lastPosition = this.parentElement.getAttribute("data-position");
-}
+// function touchStart (event) {
+//     const initialLocation = event.targetTouches[0];
+//     this.style.position = "relative";
+//     this.style.zIndex = 100;
+//     offsetX = this.getBoundingClientRect().x;
+//     offsetY = this.getBoundingClientRect().y;
+//     this.style.left = (initialLocation.pageX - offsetX) + 'px';
+//     this.style.top = (initialLocation.pageY - offsetY) + 'px';
+//     lastPosition = this.parentElement.getAttribute("data-position");
+// }
 
-function touchMove (event) {
-    event.preventDefault();
-    const touchLocation = event.targetTouches[0];
-    this.style.left = (touchLocation.pageX - offsetX) + 'px';
-    this.style.top = (touchLocation.pageY - offsetY) + 'px';
-}
+// function touchMove (event) {
+//     event.preventDefault();
+//     const touchLocation = event.targetTouches[0];
+//     this.style.left = (touchLocation.pageX - offsetX) + 'px';
+//     this.style.top = (touchLocation.pageY - offsetY) + 'px';
+// }
 
-function touchEnd (event) {
-    const x = this.getBoundingClientRect().left + (width / 2);
-    const y = this.getBoundingClientRect().top + (height / 2);
+// function touchEnd (event) {
+//     const x = this.getBoundingClientRect().left + (width / 2);
+//     const y = this.getBoundingClientRect().top + (height / 2);
 
-    for (let i = 0; i < coordinates.length; i++) {
-        if (x > coordinates[i].left && x < coordinates[i].right && y > coordinates[i].top && y < coordinates[i].bottom) {
-            empties[i].append(this);
-            this.style.left = "5px";
-            this.style.top = "5px";
-            return false;
-        }
-        empties[lastPosition].append(this);
-        this.style.left = "5px";
-        this.style.top = "5px";
-    }
-}
+//     for (let i = 0; i < coordinates.length; i++) {
+//         if (x > coordinates[i].left && x < coordinates[i].right && y > coordinates[i].top && y < coordinates[i].bottom) {
+//             empties[i].append(this);
+//             this.style.left = "5px";
+//             this.style.top = "5px";
+//             return false;
+//         }
+//         empties[lastPosition].append(this);
+//         this.style.left = "5px";
+//         this.style.top = "5px";
+//     }
+// }
